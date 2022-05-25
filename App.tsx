75953bde86ca5425a83ce70457d7cccdb95f4917
alienelena <alienelenaaa@gmail.com>
2022-05-25 16:05:31 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,317 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ButtonBase from '@mui/material/ButtonBase';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import { purple } from '@mui/material/colors';
+import React, { useState } from "react";
+
+
+const Items = [
+  {
+    name: 'DJI Mini3 Pro',
+    image:
+      'https://www.bhphotovideo.com/images/images1500x1500/dji_cp_ma_00000492_02_mini_3_pro_with_1700260.jpg',
+  },
+  {
+    name: 'DJI Air 2S',
+    image:
+      'https://www.fortress.com.hk/medias/1-inch-CMOS-Sensor-12312360.jpg?context=bWFzdGVyfGZyb250em9vbXw1NDUyNnxpbWFnZS9qcGVnfGZyb250em9vbS9oNDcvaDAyLzk3MjM5MjEyMzU5OTguanBnfDBiODcxZmQ5MWRkYjA5ZDRmZDZmY2U4ZDcyOTQ2ODY4ZWJlMDA5YjEyN2ZlNjRkNjgzMzUxZGRjN2NkNGRmMjc',
+  },
+  {
+    name: 'DJI FPV',
+    image:
+      'https://www.scandinavianphoto.se/globalassets/1050797.jpg?ref=81F07EDA5A&w=960&h=960&mode=max',
+  },
+];
+
+const conditions = [
+  {
+    size: 'small',
+    color: 'success',
+    variant: 'contained',
+    conditionName: '良品',
+  },
+  {
+    size: 'small',
+    color: 'secondary',
+    variant: 'contained',
+    conditionName: '二手',
+  },
+  {
+    size: 'small',
+    color: 'error',
+    variant: 'contained',
+    conditionName: '不良',
+  },
+  {
+    size: 'small',
+    variant: 'contained',
+    conditionName: '缺貨',
+  },
+  {
+    size: 'small',
+    variant: 'outlined',
+    conditionName: '發霉',
+  },
+];
+
+const images = [
+  {
+    url:'https://cdn.shopify.com/s/files/1/0253/9057/9791/files/banner-bg-70e8e8aba7fefef8156830bddbc3f151_1024x1024.jpg?v=1567792030',
+    title:'DJI Home Banner',
+    width:'100%'
+  },
+];
+
+const App = () => {
+
+  const [clickedButton, setClickedButton] = useState('');
+
+  const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    const button: HTMLButtonElement = event.currentTarget;
+    setClickedButton(button.name);
+  };
+//
+  const ImageButton = styled(ButtonBase)(({ theme }) => ({
+    position: 'relative',
+    height: 200,
+    [theme.breakpoints.down('sm')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &.Mui-focusVisible': {
+      zIndex: 1,
+      '& .MuiImageBackdrop-root': {
+        opacity: 0.15,
+      },
+      '& .MuiImageMarked-root': {
+        opacity: 0,
+      },
+      '& .MuiTypography-root': {
+        border: '4px solid currentColor',
+      },
+    },
+  }));
+  
+  const Image = styled('span')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  }));
+  
+  const ImageBackdrop = styled('span')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  }));
+  
+  const ImageMarked = styled('span')(({ theme }) => ({
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  }));
+
+  const ImageSrc = styled('span')({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  });
+//
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraAltIcon />
+          <Typography variant="h6">DJI</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pb: 6,
+          }}
+        >
+          <div>
+
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+            {images.map((image) => (
+          <ImageButton
+          focusRipple
+          key={image.title}
+            style={{
+              width: image.width,
+            }}
+          >
+          <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+          <ImageBackdrop className="MuiImageBackdrop-root" />
+          <Image>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              sx={{
+                position: 'relative',
+                p: 4,
+                pt: 2,
+                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+              }}
+            >
+              {image.title}
+              <ImageMarked className="MuiImageMarked-root" />
+            </Typography>
+          </Image>
+        </ImageButton>
+      ))}
+    </Box>
+
+            <Grid
+              container
+              spacing={1}
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography
+                  variant="h4"
+                  justify
+                  content="flex-end"
+                  color="textPrimary"
+                  gutterButton
+                >
+                  DJI SCAN CHECK PROGRAM
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Grid item xs={1}></Grid>
+                  <Grid item xs>
+                    <Typography
+                      variant="h6"
+                      align="center"
+                      color="textPrimary"
+                      paragraph
+                    >
+                      Gather precise plant-level data using the P4 Multispectral
+                      – a high-precision drone with a seamlessly integrated
+                      multispectral imaging system built for agriculture
+                      missions, environmental monitoring, and more.
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={1}></Grid>
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                alignItems="center"
+                justifyContent="center"
+                spacing={1}
+              >
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    More
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Contact US
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </div>
+        </Box>
+      </main>
+
+      <Grid Container>
+       <Typography size="small" color="textPrimary" align="center" spacing={2}>
+        {clickedButton !== ""
+          ? `You have clicked "${clickedButton}"` 
+          : "No button clicked yet"}
+        </Typography>
+      </Grid>
+
+      <Grid
+        direction="row"
+        container
+        spacing={6}
+        justifyContent="center"
+        alignItems="center"
+      >
+        {Items.map(({ name, image }, index) => (
+          <Grid key={index} item>
+            <Card flex={1}>
+              <CardMedia
+                component="img"
+                height="250"
+                image={image}
+                alt={name}
+              />
+              <CardContent>
+                <Typography variant="h5" color="textPrimary">
+                  {name}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                {conditions.map(({ conditionName, ...props }, index) => (
+                  <Button key={index} {...props} onClick={buttonHandler} className="button" name={name+conditionName}>
+                    {conditionName}
+                  </Button>
+                ))}
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-uqa1ks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-uqa1ks)
